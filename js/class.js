c4b3e57a6cd1e6517492c9d2fdc229c980db2769
Hyunseo Cho <hc8756@rit.edu>
2020-04-26 21:42:16 -0400

    completed single level game

--- js/class.js
+++ js/class.js
@@ -15,8 +15,7 @@ class Brick extends PIXI.Graphics{
 class Paddle extends PIXI.Graphics{
     constructor(x=0,y=0){
         super();
-        this.beginFill(0xFF0000);
-        this.lineStyle(5,0xFFFFFF);
+        this.beginFill(0xFFFFFF);
         this.drawRect(x,y,150,20);
         this.endFill();
         this.x=x;
@@ -32,7 +31,19 @@ class Ball extends PIXI.Graphics{
         this.endFill();
         this.x=x;
         this.y=y;
-        this.isAlive=true;
+        this.radius=10;
+        this.speed=400;
+        this.fwd={x:0,y:-1};
     }
+    move(dt=1/60){
+        this.x+=this.fwd.x*this.speed*dt;
+        this.y+=this.fwd.y*this.speed*dt;
 
+    }
+    reflectX(){
+        this.fwd.x*=-1;
+    }
+    reflectY(){
+        this.fwd.y*=-1;
+    }
 }
--- js/script.js
+++ js/script.js
@@ -14,20 +14,19 @@ let stage;
 
 //game variables
 let startScene;
-let gameScene,paddle;
+let gameScene,paddle,ball,scoreLabel,lifeLabel;
 let gameOverScene;
-
+let gameWinScene;
+//anything related to first bool is just to fix scope related mistake- ignore
+let first=true;
 
 //things we'll probably need for brick break
 let keys=[];
-let balls = []; //balls to bounce 
 let bricks = []; //bricks on the level
-let time = 0; //time 
 let score = 0; //score amount 
 let lives = 3; //player starts with only 3 lives
 //let paused = true; //pausing 
 //let levelNum = 1;
-let bricksLeft = 0; //for bricks left to hit
 
 
 //setting up the game
@@ -45,13 +44,19 @@ function setup() {
     gameOverScene= new PIXI.Container();
     gameOverScene.visible=false;
     stage.addChild(gameOverScene);
-	// #4 - Create labels for all 3 scenes
+    // #4 - Create the `gameWin` scene and make it invisible
+    gameWinScene= new PIXI.Container();
+    gameWinScene.visible=false;
+    stage.addChild(gameWinScene);
+	// #4 - Create labels for all 4 scenes
     createLabelsAndButtons();
-    app.ticker.add(gameLoop);
     // #5 - keyboard event handlers
     window.addEventListener("keydown", KeysDown);
     window.addEventListener("keyup",KeysUp);
+    
 }
+
+//helper functions to detect keyboard input
 function KeysDown(e){
     keys[e.keyCode]=true;
 }
@@ -64,6 +69,7 @@ function createLabelsAndButtons(){
         fill: 0xFF0000,
         fontSize: 48,
         fontFamily: 'Futura'});
+    //set up startscene
     let startButton= new PIXI.Text("Start Game");
     startButton.style=buttonStyle;
     startButton.x=225;
@@ -71,8 +77,6 @@ function createLabelsAndButtons(){
     startButton.interactive=true;
     startButton.buttonMode=true;
     startButton.on("pointerup",startGame);
-    //startButton.on('pointerover',e=>e.target.alpha=0.7);
-    //startButton.on('pointerout',e=>e.target.alpha=1.0);
     startScene.addChild(startButton);
 
     //set up gamescene
@@ -83,24 +87,20 @@ function createLabelsAndButtons(){
         stroke: 0xFF0000,
         strokeThickness:4
     });
-    let scoreLabel= new PIXI.Text("score: "+score);
+    scoreLabel= new PIXI.Text("score: "+score);
     scoreLabel.style= textStyle;
     scoreLabel.x=5;
     scoreLabel.y=5;
     gameScene.addChild(scoreLabel);
-    //increaseScoreBy(0);
 
-    let lifeLabel= new PIXI.Text("lives: "+lives);
+    lifeLabel= new PIXI.Text("lives: "+lives);
     lifeLabel.style=textStyle;
     lifeLabel.x=5;
     lifeLabel.y=26;
     gameScene.addChild(lifeLabel);
-    //decreaseLifeBy(0);
 
-    // 3 - set up `gameOverScene`
-    // 3A - make game over text
-    /*
-    let gameOverText = new PIXI.Text("Game Over!\n        :-O");
+    //set up gameoverscene
+    let gameOverText = new PIXI.Text("Game Over!");
     textStyle = new PIXI.TextStyle({
         fill: 0xFFFFFF,
         fontSize: 64,
@@ -113,49 +113,77 @@ function createLabelsAndButtons(){
     gameOverText.y = sceneHeight/2 - 160;
     gameOverScene.addChild(gameOverText);
 
-    // 3B - make "play again?" button
     let playAgainButton = new PIXI.Text("Play Again?");
     playAgainButton.style = buttonStyle;
     playAgainButton.x = 150;
     playAgainButton.y = sceneHeight - 100;
     playAgainButton.interactive = true;
     playAgainButton.buttonMode = true;
-    playAgainButton.on("pointerup",startGame); // startGame is a function reference
-    playAgainButton.on('pointerover',e=>e.target.alpha = 0.7); // concise arrow function with no brackets
-    playAgainButton.on('pointerout',e=>e.currentTarget.alpha = 1.0); // ditto
-    gameOverScene.addChild(playAgainButton);*/
-    
+    playAgainButton.on("pointerup",startGame);
+    gameOverScene.addChild(playAgainButton);
+
+    //set up gamewinscene
+    let gameWinText = new PIXI.Text("You win!");
+    gameWinText.style = textStyle;
+    gameWinText.x = 100;
+    gameWinText.y = sceneHeight/2 - 160;
+    gameWinScene.addChild(gameWinText);
+
+    let playAgainButton2 = new PIXI.Text("Play Again?");
+    playAgainButton2.style = buttonStyle;
+    playAgainButton2.x = 150;
+    playAgainButton2.y = sceneHeight - 100;
+    playAgainButton2.interactive = true;
+    playAgainButton2.buttonMode = true;
+    playAgainButton2.on("pointerup",startGame);
+    gameWinScene.addChild(playAgainButton2);
+
 }
+
 function startGame(){
     startScene.visible=false;
     gameOverScene.visible=false;
+    gameWinScene.visible=false;
     gameScene.visible=true;
     score=0;
     lives=3;
-   
+   //create new bricks 
     for(let i=0;i<7;i++){
-        for(let j=0; j<10;j++){
+        for(let j=0; j<3;j++){
             let b= new Brick(50*i,15*j+35);
             bricks.push(b);
             gameScene.addChild(b);
         }
     }
-    for(let i=0;i<3;i++){
-        let b= new Ball(sceneWidth/2-185,sceneHeight/2-15);
-        balls.push(b);}
-        
-    gameScene.addChild(balls[0]);
+    //create paddle
     paddle= new Paddle(sceneWidth/2-225,sceneHeight/2-10);
     gameScene.addChild(paddle);
-
+    //create ball
+    ball= new Ball(paddle.x+35,sceneHeight/2-20);
+    gameScene.addChild(ball);
+    //call gameloop function
+    if(first==true){app.ticker.add(gameLoop);}
+    else{gameLoop();}
+    
+    
 }
 
+//helper function to detect collision
+function rectsIntersect(a,b){
+    var ab = a.getBounds();
+    var bb = b.getBounds();
+    return ab.x + ab.width > bb.x && ab.x < bb.x + bb.width && ab.y + ab.height > bb.y && ab.y < bb.y + bb.height;
+    
+}
 
 function gameLoop(){
-
-	// #1 - Calculate "delta time"
+    first=false;
+    // #1 - Calculate "delta time"
 	let dt = 1/app.ticker.FPS;
     if (dt > 1/12) dt=1/12;
+    //keep updating labels
+    lifeLabel.text=`lives: ${lives}`;
+    scoreLabel.text=`score: ${score}`;
 	// #2 - Move paddle with A and D keys
     if(keys["68"] && paddle.x<sceneWidth/2+75){
         paddle.x+=5;
@@ -163,8 +191,76 @@ function gameLoop(){
     if(keys["65"] && paddle.x>-150){
         paddle.x-=5;
     }
-	
-	
+
+    // #3 - Move the ball around 
+    ball.move(dt);
+    // #3A - Detect collision with bricks
+	for(var i=0;i<bricks.length;i++){
+        if(rectsIntersect(bricks[i],ball) && bricks[i].isAlive){
+            gameScene.removeChild(bricks[i]);
+            bricks[i].isAlive=false;
+            score+=100;
+            ball.reflectX();
+            ball.reflectY();
+            
+        }
+    }
+    // #3B - Detect collision with paddle
+    if(rectsIntersect(paddle,ball)){
+        ball.reflectY();
+        if(keys["65"]){
+            ball.fwd.x-=0.5;
+        }
+        else if(keys["68"]){
+            ball.fwd.x+=0.5;
+        }
+    }
+    // #3C - Detect collision with walls
+    if(ball.x<=ball.radius-170||ball.x>=sceneWidth-ball.radius-165){
+        ball.reflectX();}
+    if(ball.y<=ball.radius-350){
+        ball.reflectY();}
+    // #3D - When ball goes out of bounds
+    if(ball.y>=sceneHeight-ball.radius-200){
+        //reset position of ball and paddle, decrease life
+        paddle.x=sceneWidth/2-225;
+        paddle.y=sceneHeight/2-10;
+        ball.x= paddle.x+35;
+        ball.y= sceneHeight/2-20;
+        ball.fwd={x:0,y:-1};
+        lives-=1;
+        
+    }
+    //game over
+    if (lives<=0){
+        gameOverScene.visible=true;
+        gameScene.visible=false;
+        end();
+        return;
+        }
+
+    else if(ifWin()){
+        gameWinScene.visible=true;
+        gameScene.visible=false;
+        end();
+        return;
+
+    }
 }
+
+function ifWin(){
+    for(var i=0;i<bricks.length;i++){
+        if(bricks[i].isAlive==true){
+            return false;
+        }
+    }
+    return true;
+}
+function end(){
+    bricks.forEach(c=>gameScene.removeChild(c));
+    bricks=[];
+    gameScene.removeChild(ball);
+    gameScene.removeChild(paddle);
+    }
 
 setup();
